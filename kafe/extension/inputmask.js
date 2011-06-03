97403bfa1cff00bb83829d35dcb4126aac570c24
Jb Landry <jb@jblandry.info>
2011-06-03 13:53:12 +0000

    ADD - ext.inputmask
    git-svn-id: file:///Users/admabso/GitConversions/local-svn/KAFE/trunk@2338 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ kafe/extension/inputmask.js
@@ -0,0 +1,65 @@
+//-------------------------------------------
+// kafe.ext.inputmask
+//-------------------------------------------
+kafe.extend({name:'inputmask', version:'1.0', obj:(function($,K,undefined){
+
+	//-------------------------------------------
+	// PRIVATE
+	//-------------------------------------------
+	var __masks = {
+		datetime:   'y-m-d h:i',
+		date:       'y-m-d',
+		time:       'h:i',
+		postalcode: 'a9a 9a9',
+		phone:      '(999) 999-9999',
+		phoneext:   '(999) 999-9999 x9999'			
+	};
+
+	var d60 = {
+		validator:    '[0-5][0-9]',
+		cardinality:  2,
+		prevalidator: [{
+			validator: '[0-5]', 
+			cardinality: 1
+		}]
+    };
+
+	$.extend($.inputmask.defaults.definitions, {
+		
+		// hour
+		h: {
+			validator:    '[01][0-9]|2[0-3]',
+			cardinality:  2,
+			prevalidator: [{
+				validator: '[0-2]', 
+				cardinality: 1
+			}]
+	    },
+	
+		// minute
+		i: d60,
+		
+		// seconds
+		s: d60
+	});
+	
+	
+	
+
+	
+	//-------------------------------------------
+	// PUBLIC
+	//-------------------------------------------
+	var inputmask = {};
+	
+	// getMask (name)
+	// return predefined mask
+	//-------------------------------------------
+	inputmask.getMask = function(name) {
+		return (!!__masks[name]) ? __masks[name] : '';
+	};
+	
+
+	return inputmask;
+
+})(jQuery,kafe)});
