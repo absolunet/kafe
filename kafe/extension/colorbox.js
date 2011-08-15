ab10dcf4f4d9caffbce7c89608aab5a7937d337c
Léandre Brière-Côté <lbcote@absolunet.com>
2011-08-15 17:30:49 +0000

    [No message]

--- kafe/extension/colorbox.js
+++ kafe/extension/colorbox.js
@@ -1,27 +1,28 @@
 //-------------------------------------------
 // kafe.ext.colorbox
 //-------------------------------------------
-kafe.extend({name:'colorbox', version:'0.2', obj:(function($,K,undefined){
+kafe.extend({name:'colorbox', version:'1.0', obj:(function($,K,undefined){
 
 	//default params
 	var _params = {
-		inline:true, 
-		opacity:0.9, 
-		transition:'none'
+		opacity:0.5, 
+		transition:'none',
+		overlayClose:true,
+		escKey:true,
+		scrolling:false
 	};
-	
 	
 	//-------------------------------------------
 	// PUBLIC
 	//-------------------------------------------
 	var colorbox = {};
 	
-	// getParams ([id])
-	// return default params
+	// getParams ([options])
+	// return default params with optional extra params
 	//-------------------------------------------
-	colorbox.getParams = function(id) {
-		var p = _params
-		if (!!id) { p.id = '#'+id; }
+	colorbox.getParams = function(options) {
+		var p = (!!options) ? options : {};
+		$.extend(p, _params);
 		return p;
 	};
 	
@@ -29,7 +30,8 @@ kafe.extend({name:'colorbox', version:'0.2', obj:(function($,K,undefined){
 	// set default params
 	//-------------------------------------------
 	colorbox.setParams = function() {
-		_params = arguments[0];
+		$.extend(_params, arguments[0]);
+		//_params = arguments[0];
 	};
 
 	// display (string)
@@ -37,18 +39,60 @@ kafe.extend({name:'colorbox', version:'0.2', obj:(function($,K,undefined){
 	//-------------------------------------------
 	colorbox.display = function(string) {
 		$('#cb-display').html(string);
-		$.colorbox(this.getParams('cb-display'));
+		$.colorbox( this.getParams({ id:'#cb-display' }) );
 	};
 
-	// moveInForm
+	// moveInForm ()
 	// move colorbox in webform
 	//-------------------------------------------
 	colorbox.moveInForm = function() {
 		$("#colorbox").appendTo('form');
 	};
 	
-	// more to come from Leanders
+	// dialog ( content, [commands] )
+	// opens a message window with custom buttons
+	//-------------------------------------------
+	colorbox.dialog = function( content, commands ) {
+		
+		var html = '<div id="kafe-colorbox-dialog">' + content;
 
+		if (commands == undefined || commands.length == 0)
+			commands = [{ label:'OK', callback:function(){ $.colorbox.close(); } }];
+
+		html += '<div class="commands" style="white-space:nowrap;">';
+		$.each(commands, function(i, btn){
+			html += '<a href="javascript:void(0);" class="Btn">' + btn.label + '</a>';
+		});
+		html += '</div></div>';
+
+		var content = $(html);
+		$.each(commands, function(i, btn){
+			content.find('.Btn:eq(' + i + ')').bind('click', (typeof btn.callback === 'function') ? btn.callback : function(){ $.colorbox.close(); });
+		});
+		
+		$.colorbox( this.getParams({ html:content }) );
+		
+	};
+
+	// confirm ( selector, message )
+	// simulate a confirm() behavior using colorbox.dialog
+	//-------------------------------------------
+	colorbox.confirm = function( selector, message, OKLabel, CancelLabel ) {
+		kDialog = this;
+		$(function(){
+			$(selector).bind('click', function(e) {
+				e.preventDefault();
+				var $this = $(this);
+				kDialog.dialog( message, [
+					{ label:OKLabel , callback: function() {
+						eval($this.attr('href'));
+						$.colorbox.close();
+					}},
+					{ label:CancelLabel }
+				]);
+			});
+		});
+	}
 
 	return colorbox;
 
--- kafe/form.js
+++ kafe/form.js
@@ -115,6 +115,83 @@ kafe.bonify({name:'form', version:'1.2', obj:(function($,K,undefined){
 		;
 	};
 
+	// passwordStrength (elements, [callback])
+	// calculates the password strength value of given fields
+	//-------------------------------------------
+	form.passwordStrength = function(elements, callback) {
+		
+		var min_length = 6;
+		
+		var _countRegexp = function (val, rex) {
+			var match = val.match(rex);
+			return match ? match.length : 0;
+		};
+		var _getStrength = function (val, minLength) {
+			var len = val.length;
+
+			// too short =(
+			if (len < minLength) {
+				return 0;
+			}
+
+			var nums = _countRegexp(val, /\d/g),
+				lowers = _countRegexp(val, /[a-z]/g),
+				uppers = _countRegexp(val, /[A-Z]/g),
+				specials = len - nums - lowers - uppers;
+
+			// just one type of characters =(
+			if (nums == len || lowers == len || uppers == len || specials == len) {
+				return 1;
+			}
+
+			var strength = 0;
+			if (nums) { strength += 2; }
+			if (lowers) { strength += uppers ? 4 : 3; }
+			if (uppers) { strength += lowers ? 4 : 3; }
+			if (specials) { strength += 5; }
+			if (len > 10) { strength += 1; }
+
+			return strength;
+		};
+		var _getStrengthLevel = function (val, minLength) {
+			var strength = _getStrength(val, minLength),
+				val = 1;
+			if (strength <= 0) {
+				val = 1;
+			} else if (strength > 0 && strength <= 4) {
+				val = 2;
+			} else if (strength > 4 && strength <= 8) {
+				val = 3;
+			} else if (strength > 8 && strength <= 12) {
+				val = 4;
+			} else if (strength > 12) {
+				val = 5;
+			}
+
+			return val;
+		};
+		
+		$(elements)
+			.bind('input paste cut keyup',function(e) {
+			
+				var $this = $(this),
+					delay = ($.browser.msie & parseInt($.browser.version) <= 8) ? 1 : 0;
+				
+				setTimeout(function(){
+					var val = $this.val(),
+						strength = _getStrengthLevel(val, min_length);
+					
+					if ($.isFunction(callback)) {
+						callback(strength);
+					}
+					
+				},delay);
+			})
+		;
+	};
+
+
+
 	return form;
 
 })(jQuery,kafe)});
