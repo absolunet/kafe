a39e8d6a333bf7ea812fe1f11f03b36d7d197c74
Jb Landry <jb@jblandry.info>
2012-11-05 14:28:18 +0000

    ADD - Ajout module array
    git-svn-id: file:///Users/admabso/GitConversions/local-svn/KAFE/trunk@30281 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ kafe/array.js
@@ -0,0 +1,31 @@
+//-------------------------------------------
+// kafe.array
+//-------------------------------------------
+kafe.bonify({name:'array', version:'0.1', obj:(function(K,undefined){
+	var $ = K.jQuery;
+	
+	//-------------------------------------------
+	// PUBLIC
+	//-------------------------------------------
+	var array = {};
+
+	// randomize (array)
+	// randomize array order (fisherYates)
+	//-------------------------------------------
+	array.randomize = function(myArray) {
+		var i = myArray.length;
+			if ( i == 0 ) return false;
+			while ( --i ) {
+				var j = Math.floor( Math.random() * ( i + 1 ) );
+				var tempi = myArray[i];
+				var tempj = myArray[j];
+				myArray[i] = tempj;
+				myArray[j] = tempi;
+			}
+			return myArray;
+		}
+	};
+
+	return array;
+
+})(kafe)});
