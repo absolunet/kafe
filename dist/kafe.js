bc6d3f1d6eb9f8507433cb1f0bd55a9ddc0da9a1
Jb Landry <jb@jblandry.info>
2016-11-02 17:14:40 -0400

    Version bump + jQuery 3

--- dist/kafe.js
+++ dist/kafe.js
@@ -1,5 +1,5 @@
 /*!
- * kafe 3.1.6
+ * kafe 3.2.0
  * http://absolunet.github.io/kafe
  *
  * Copyright 2011-2016 Absolunet, http://absolunet.com/
@@ -52,7 +52,7 @@
 
 
 		/**
-		* ### Version 3.1.6
+		* ### Version 3.2.0
 		* kafe core
 		*
 		* @module kafe
@@ -66,7 +66,7 @@
 			* @property VESYON
 			* @type String
 			**/
-			VESYON: '3.1.6',
+			VESYON: '3.2.0',
 
 			/**
 			* kafe author
